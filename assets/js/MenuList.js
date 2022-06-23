1c01ce0bc1796b682af432f4cb60a1ee58aef0bb
J-JRT <88273015+J-JRT@users.noreply.github.com>
2022-06-23 18:45:54 +0700

    Add files via upload

---
+++ assets/js/MenuList.js
@@ -0,0 +1,194 @@
+endpoint = `https://api.phamvandien.xyz`
+message_list = [{
+    icon: 'fa-solid fa-star-exclamation',
+    title: 'Trang web bắt đầu hoạt động',
+    timing: '1655701877368'
+}];
+
+item_list = [{
+        name: 'TikTok',
+        icon: 'fa-brands fa-tiktok',
+        item: [{
+                "url": endpoint + "/tiktok?url=https://vm.tiktok.com/ZMLMbkp82",
+                "name": "TikTok Downloader"
+            },
+            {
+                "url": endpoint + "/tiktok?search=son tung",
+                "name": "TikTok Search"
+            },
+            {
+                "url": endpoint + "/tiktok?username=ltkh2004",
+                "name": "TikTok Info User"
+            }
+        ]
+    },
+    {
+        name: 'SoundCloud',
+        icon: 'fa-brands fa-soundcloud',
+        item: [{
+                "url": endpoint + '/soundcloud?search=son tung',
+                "name": "SoundCloud Search"
+            },
+            {
+                "url": endpoint + '/soundcloud?url=https://soundcloud.com/sontungmtp-music/ch-ng-ta-c-a-hi-n-t-i',
+                "name": "SoundCloud Downloader"
+            }
+        ]
+    },
+    {
+        name: 'Youtube',
+        icon: 'fa-brands fa-youtube',
+        item: [{
+                "url": endpoint + '/youtube?search=son tung',
+                "name": "Youtube Search"
+            },
+            {
+                "url": endpoint + '/youtube?GetVideoId=https://www.youtube.com/watch?v=Jk38OqdAQxc',
+                "name": "Get Video Id"
+            },
+            {
+                "url": endpoint + '/youtube?GetVideoDetails=xypzmu5mMPY',
+                "name": "Get Video Details"
+            },
+            {
+                "event": 'Some Error',
+                "url": endpoint + '/youtube?downloadVideo=Jk38OqdAQxc',
+                "name": "Youtube Downloader"
+            }
+        ]
+    },
+    {
+        name: 'Short Url',
+        icon: 'fa-solid fa-curling-stone',
+        item: [{
+            "url": endpoint + '/slink?url=https://www.youtube.com/watch?v=Jk38OqdAQxc',
+            "name": "Short Url"
+        }]
+    },
+    {
+        name: 'Pinterest',
+        icon: 'fa-brands fa-pinterest',
+        item: [{
+            "url": endpoint + '/pinterest?search=mirai',
+            "name": "Search"
+        }]
+    },
+    {
+        name: 'Random Images',
+        icon: 'fa-solid fa-images',
+        item: [{
+                "event": 'NSFW',
+                "url": endpoint + '/images/ausand',
+                "name": "Ausand"
+            },
+            {
+                "event": 'NSFW',
+                "url": endpoint + '/images/du',
+                "name": "NSFW 1"
+            },
+            {
+                "event": 'NSFW',
+                "url": endpoint + '/images/girl',
+                "name": "NSFW 2"
+            },
+            {
+                "event": 'NSFW',
+                "url": endpoint + '/images/mong',
+                "name": "NSFW 3"
+            },
+            {
+                "event": 'NSFW',
+                "url": endpoint + '/images/anime',
+                "name": "Anime"
+            }
+        ]
+    },
+    {
+        name: 'Simsimi',
+        icon: 'fa-solid fa-message-bot',
+        item: [{
+                "url": endpoint + '/sim?type=ask&ask=sim ơi',
+                "name": "Ask"
+            },
+            {
+                "url": endpoint + '/sim?type=teach&ask=sim ơi&ans=ơii',
+                "name": "Teach"
+            }
+        ]
+    },
+    {
+        name: 'Games',
+        icon: 'fa-solid fa-gamepad',
+        item: [{
+                "url": endpoint + '/game/dovui',
+                "name": "Đố vui"
+            },
+            {
+                "url": endpoint + '/game/dovuiv2',
+                "name": "Đố vui 2"
+            },
+            {
+                "url": endpoint + '/game/dhbcv1',
+                "name": "Đuổi hình bắt chữ"
+            },
+            {
+                "url": endpoint + '/game/dhbcv1',
+                "name": "Đuổi hình bắt chữ 2"
+            },
+            {
+                "url": endpoint + '/game/linkword?word=ngu ngốc',
+                "name": "Nối từ"
+            },
+            {
+                "url": endpoint + '/game/taixiu',
+                "name": "Tài xỉu"
+            },
+            {
+                "url": endpoint + '/game/v2/taixiu',
+                "name": "Tài xỉu 2"
+            },
+            {
+                "url": endpoint + '/game/vuatiengviet',
+                "name": "Vua tiếng Việt"
+            },
+            {
+                "url": endpoint + '/vuatiengviet/image?word=ngu ngốc',
+                "name": "Vua tiếng Việt 2"
+            }
+        ]
+    },
+    {
+        name: 'Tiện ích',
+        icon: 'fa-solid fa-lamp-street',
+        item: [{
+                "url": endpoint + '/imgur?link=https://haycafe.vn/wp-content/uploads/2022/02/Anh-gai-xinh-Viet-Nam.jpg',
+                "name": "Imgur Upload"
+            },
+            {
+                "url": endpoint + '/random/useragent',
+                "name": "Random User-Agent"
+            },
+            {
+                "url": endpoint + '/covid?country=viet%20nam',
+                "name": "COVID 19 DATA"
+            }
+        ]
+    },
+    {
+        name: 'Facebook',
+        icon: 'fa-brands fa-facebook',
+        item: [{
+            "url": endpoint + '/finduid?url=https://www.facebook.com/PhamVanDien.User/',
+            "name": "Find UID"
+        }]
+    },
+    {
+        name: 'Liên hệ Admin',
+        icon: 'fas fa-user-headset',
+        item: [{
+            event: 'DO NOT SPAM',
+            url: 'https://www.facebook.com/PhamVanDien.User/',
+            name: 'Facebook'
+        }]
+    }
+];
